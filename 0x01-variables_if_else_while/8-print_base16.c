d8f31c411c3db84fc085b6df374bd868b40f00b5
Bimbola <bashorunbimbola@gmail.com>
2022-09-10 17:48:34 -0700

    base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 /**
- *
  * main - Entry point
  *
  * return: Always return 0
- *
  */
 int main(void)
 {
 	unsigned char a = '0';
 	int i;
+	
+
 	for (i = 0; i < 10; i++)
 	{
 		putchar(a);
 		a++;
 	}
-	a ='i';
+	a = '1';
 	for (i = 0; i < 6; i++)
 	{
 		putchar('0' + a);
