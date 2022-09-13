b44029ea1365f98b708dfaf3ad5269dded3cc2f7
Bimbola <bashorunbimbola@gmail.com>
2022-09-13 15:15:01 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: This programs prints an alphabet from a-z
+ *
+ * Return: Return always 0
+ */
+
+void print_alphabet(void)
+{
+	int ch
+	for (ch = 'a'; ch <= 'z'; ch++)
+	putchar();
+	return (0);
+}
