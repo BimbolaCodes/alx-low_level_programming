5ae4ed4ee528bf9135c3ac032dc60eb02b46f6e0
Bimbola <bashorunbimbola@gmail.com>
2022-09-13 18:39:02 -0700

    Xalphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print function - prints the alphabet in lowercase
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void);
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+putchar('\n');
+}
+
