de176dc4e62db64ab31701644a5187decd867754
Bimbola <bashorunbimbola@gmail.com>
2022-09-14 21:28:05 -0700

    times_table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,49 @@
+#include <main.h>
+/**
+ * Print_numbers -  print the time trables
+ * 
+ * Parameters
+ *
+ * @ n parameters
+ *
+ * times_table: returns 0
+ */
+ void print_times_table(int n);
+{
+int digit, mult, result;
+{
+if (n <= 15 && n >= 0
+{
+for (digit = 0; digit <= n; digit++)
+{
+_putchar('0');
+
+for (mult = 1; mult <= n; mult++)
+
+{
+_putchar(',');
+_putchar(' ');
+result = digit * mult;
+if (result <= 99)
+_putchar(' ');
+_putchar('0');
+
+if (result <= 9)
+_putchar(' ');if (result >= 100)
+{
+_putchar((result / 100) + '0');
+_putchar((result / 10) % 10 + '0');
+}
+else if (result <= 99 && result >= 10)
+{
+_putchar((result / 10) + '0');
+}
+_putchar((result % 10) + '0');
+
+}
+
+_putchar('\n');
+
+}
+}
+}
