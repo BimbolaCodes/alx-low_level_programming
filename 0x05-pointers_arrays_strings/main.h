0b9e96cadfb21fb28eb98d7b1a53f47fac8b5b0f
Bimbola <bashorunbimbola@gmail.com>
2022-09-19 20:52:56 -0700

    no2

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,11 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+int _putchar(char c);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
@@ -12,5 +14,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
-
-#endif
+#endif/*MAIN_H*/
