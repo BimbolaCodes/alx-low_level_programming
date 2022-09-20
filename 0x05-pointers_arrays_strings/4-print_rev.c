789c1421474ce45d8f940188cb14a4ee2e551e31
Bimbola <bashorunbimbola@gmail.com>
2022-09-19 18:41:43 -0700

    A good engineer thinks in reverse and asks himself about the stylistic consequences of the components and systems he proposes

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,19 +1,31 @@
 #include "main.h"
 
 /**
- * print_rev - prints a string in reverse
- * @s: the used string reference pointer
- * Return: 0
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: string to reverse
  */
 
 void print_rev(char *s)
 {
+	int len = 0;
 	int i = 0;
 
-	while (s[i])
-		i++;
+	/* iterate to find length of string and point to last character */
+	while (*s != '\0')
+	{
+		len++;
+		++s;
+	}
 
-	while (i--)
-		_putchar(s[i]);
+	/* go back to character before null character */
+	s--;
+
+	/* print string reversed */
+	for (i = len; i > 0; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
 	_putchar('\n');
 }
