8d10911a7b1ba53fd6586edf6b0255f199ca6867
Bimbola <bashorunbimbola@gmail.com>
2022-09-22 07:35:00 -0700

    8. rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,28 +1,25 @@
 #include "main.h"
 
 /**
- * leet - Write a function that encodes a string
- * @s: pointer referencing the string
- * Return: s transformed
+ * * rot13 - encodes a string into rot13
+ * @s: the string to be encoded
+ * Return: a pointer to the string
  */
 
-char *leet(char *s)
+char *rot13(char *s)
 {
-	int count, inner_count;
-	char s1[] = "aeotl";
-	char S1[] = "AEOTL";
-	char s2[] = "43071";
-
-	for (count = 0; s[count] != '\0'; count++)
+	char alphabet[53] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char nycunorg[53] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int i = 0, j;
+	
+	while (s[i])
 	{
-		for (inner_count = 0; inner_count < 5; inner_count++)
-		{
-			if (s[count] == s1[inner_count] || s[count] == S1[inner_count])
-			{
-				s[count] = s2[inner_count];
-				break;
-			}
-		}
+		j = 0;
+		while (alphabet[j] != s[i])
+			j++;
+		if (j <= 51)
+			s[i] = nycunorg[j];
+		i++;
 	}
 
 	return (s);
